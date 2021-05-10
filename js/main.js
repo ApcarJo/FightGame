aeda9b496bacd8b4579fe620af228c3d6e300bbb
ApcarJo <hansokaz@gmail.com>
2021-05-10 13:54:55 +0200

    Fixed the selection of the fighters

--- js/main.js
+++ js/main.js
@@ -22,16 +22,20 @@ const chooseFighter = (fighter) => {
         team1.push(allPlayers[fighter]);
         } else {
         team2.push(allPlayers[fighter]); 
+        if (team2.length == 2){
+            console.log("este team 1", team1);
+            console.log("este team 2", team2);
+            cambiaFase("fase3");
+            }
         }
+        
 
         document.getElementById(fighter).onclick = "";
         document.getElementById(fighter).className = "seleccionado";
-        console.log(team1);
 
-    } else {
+    } 
 
-        console.log("este team 1", team1);
-        console.log("este team 2", team2);
-        cambiaFase("fase3");
-    }
+        
+        
+    
 };

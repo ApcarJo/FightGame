78f18101d5f04d86bf98304688916a635fbf6980
ApcarJo <hansokaz@gmail.com>
2021-05-15 01:14:52 +0200

    Added preview of fighter and stats to character selection

--- js/main.js
+++ js/main.js
@@ -42,6 +42,24 @@ const chooseFighter = (fighter) => {
     }    
 };
 
+const preview = (a) => {
+    let prevL = document.getElementById("previewLeft");
+    prevL.innerHTML = `<div><img src="img/avatar${a}.jfif" alt="previewL"></div>`;
+    prevL.style.backgroundImage = `linear-gradient(to bottom, black, transparent,  black), linear-gradient(to left,black, transparent, black)`;
+    let prevR = document.getElementById("previewRight");
+    let str = JSON.stringify(allPlayers[a]);
+    prevR.innerHTML = `<div><span>${str}</span></div>`;
+    console.log(allPlayers[a]);
+};
+
+const outPreview = () => {
+    let prev = document.getElementById("previewLeft");
+    prev.innerHTML = "";
+    prev.style.backgroundImage = `linear-gradient(to bottom, black, black)`;
+    let prevR = document.getElementById("previewRight");
+    prevR.innerHTML = "";
+}
+
 // Function to show both teams on html
 const showBothTeams = () => {
     let teams = document.getElementById("teams");
@@ -72,15 +90,59 @@ const scenarioFight = (i, j) => {
     rightPlayer1.src=`img/${team2[j].nombre}.jfif`;
     let lifeP2 = document.getElementById("vidaP2");
     lifeP2.innerHTML = `${team2[j].nombre} : ${team2[j].vida}`;
-
-    // Cambio el color del fondo
-    // let fondo = document.getElementById("phase4");
-    // fondo.style.background-color = `rgb(50, 231, 0);`;
 };
 
 
 const fighting1 = () => {
+    p1 = team1[i];
+        p2 = team2[j];
+        console.log("Lucha!!!");
 
+        p1.hit(p2);
+        
+        if (p2.vida<=0 && j<1) {
+            j++;
+            document.getElementById("vidaP2").innerHTML = `${team2[j].nombre} : ${team2[j].vida}`;
+            scenarioFight(i, j);
+        }
+
+        (p2.vida<0) ? p2.vida=0 : console.log("");
+          
+        let vidaP2 = document.getElementById("vidaP2");
+        vidaP2.innerHTML = `${team2[j].nombre} : ${team2[j].vida}`;
+        let lifeBar1 = document.getElementById("fighter2Hit");
+        lifeBar1.style.width = team2[j].vida*0.008 + "em";
+
+        finishFight(i,j);
+};
+
+const fighting2 = () => {
+
+    p1 = team1[i];
+    p2 = team2[j];
+    console.log("Lucha!!!");
+
+    p2.hit(p1);
+
+    
+    if (p1.vida<=0 && i<1) {
+        i++;
+        document.getElementById("vidaP1").innerHTML = `${team1[i].nombre} : ${team1[i].vida}`;
+        scenarioFight(i, j);
+    }
+
+    (p1.vida<0) ? p1.vida=0 : console.log("");
+
+    /* Update life status after each hit */
+    let vidaP1 = document.getElementById("vidaP1");
+    vidaP1.innerHTML = `${team1[i].nombre} : ${team1[i].vida}`;
+    let lifeBar = document.getElementById("fighter1Hit");
+    lifeBar.style.width = team1[i].vida*0.008 + "em";
+
+    finishFight(i,j);
+};
+
+const finishFight = (i,j) => {
     if ((team1[i].vida<=0 && i==1) || (team2[j].vida<=0 && j==1)){
         let winner = document.getElementById("winner");
 
@@ -100,106 +162,9 @@ const fighting1 = () => {
     
         setTimeout(()=> {
             phaseChanger("phase5");
-        }, 2000);
-
-    } else {
-
-        p1 = team1[i];
-        p2 = team2[j];
-        console.log("Lucha!!!");
-
-        p1.hit(p2);
-        
-        if (p2.vida<=0 && j<1) {
-            j++;
-            document.getElementById("vidaP2").innerHTML = `${team2[j].nombre} : ${team2[j].vida}`;
-            scenarioFight(i, j);
-        }
-
-        (p2.vida<0) ? p2.vida=0 : console.log("bicho");
-          
-        let vidaP2 = document.getElementById("vidaP2");
-        vidaP2.innerHTML = `${team2[j].nombre} : ${team2[j].vida}`;
-        let lifeBar1 = document.getElementById("fighter2Hit");
-        lifeBar1.style.width = team2[j].vida*0.0066+1+ "em";
+        }, 0000);
     }
-};
-
-const fighting2 = () => {
-
-    if ((team1[i].vida<=0 && i==1) || (team2[j].vida<=0 && j==1)){
-        let winner = document.getElementById("winner");
-
-        if (team1[i].vida<=0 && team2[j].vida<=0) { 
-        winner.innerHTML = `¡¡Double KO!!`;
-
-        } else if (team2[j].vida<=0) {
-            winner.innerHTML = `
-            <img  src="img/${team1[i].nombre}.jfif">
-            ${team1[i].nombre} has WON!!`;    
-            
-        } else{
-            winner.innerHTML = `
-            <img src="img/${team2[j].nombre}.jfif">
-            ${team2[j].nombre} has WON!!`;
-        }
-    
-        setTimeout(()=> {
-            phaseChanger("phase5");
-        }, 2000);
-
-    } else {
-        p1 = team1[i];
-        p2 = team2[j];
-        console.log("Lucha!!!");
-
-        p2.hit(p1);
-
-        
-        if (p1.vida<=0 && i<1) {
-            i++;
-            document.getElementById("vidaP1").innerHTML = `${team1[i].nombre} : ${team1[i].vida}`;
-            scenarioFight(i, j);
-        }
-
-        (p1.vida<0) ? p1.vida=0 : console.log("bicho");
-
-        /* Update life status after each hit */
-        let vidaP1 = document.getElementById("vidaP1");
-        vidaP1.innerHTML = `${team1[i].nombre} : ${team1[i].vida}`;
-        let lifeBar = document.getElementById("fighter1Hit");
-        lifeBar.style.width = team1[i].vida*0.0066+1 + "em";
-    }
-};
-
-
-const animationHit = (i, j) => {
-    let lucha = document.getElementById("lucha");
-    lucha.innerHTML = `
-    <div class="teamCharacters">
-        <div class="lifeBar">
-        <div class="lifeBarP1" id="fighter1Hit"></div>
-        </div>
-        <div><img class="scenarioFight" id="attack1" alt="luchador3"></div>
-        <span class="textoBasico" id="vidaP1">${team1[i].nombre} : ${team1[i].vida}</span> 
-
-    </div>
-    <div class="teamCharacters">
-    <div class="fightPanel" alt="lucha"><img class="fotoVersus" src="img/fight.png"></div>
-    </div>
-    <div class="teamCharacters">
-        <div class="lifeBar">
-        <div class="lifeBarP2" id="fighter2Hit"></div>
-        </div>
-        <div><img class="scenarioFight" id="fighter2Hit" src="img/${team2[j].nombre}.jfif" alt="luchador3"></div>
-        <span class="textoBasico" id="vidaP2">${team2[j].nombre} : ${team2[j].vida}</span>
-        
-    </div>`;
-
-    // Cambio el color del fondo
-    let fondo = document.getElementById("phase4");
-    fondo.style.backgroundColor = "green";
-};
+}
 
 document.body.onkeydown = (e) =>{
     if(e.keyCode == 81){
@@ -240,17 +205,6 @@ document.body.onkeyup = (e) =>{
     }
 }
 
-// while (!document.body.onkeydown){
-//     for (let a=1; a<=4; a++){
-//         (function (a) {
-//             setTimeout(function () {
-//                 document.getElementById("attack1").src=`img/MH2_Idle/Idle${a}.png`;
-//             }, 75*a);
-//           })(a);
-//     }
-// }
-
-
 const reset = document.getElementById('playAgain');
 
 reset.addEventListener('click', () => {
@@ -261,6 +215,6 @@ reset.addEventListener('click', () => {
 
 // Añadir un contador de combate que resuelva ganador al equipo que vaya ganando cuando el tiempo llega a 0.
 
-// Arreglar lo de la barra de vida
+// Arreglar lo de la barra de vida - DONE
 
-// Cambiar la estructura de los divs de las phases para que no se resetee la barra de vida cuando un jugador es derrotado.
+// Cambiar la estructura de los divs de las phases para que no se resetee la barra de vida cuando un jugador es derrotado. - DONE
